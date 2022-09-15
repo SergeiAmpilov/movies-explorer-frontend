ec6afb73c5bfdcf6e1d034203a95851c796af7ac
Sergei <sergei.ampilov@gmail.com>
2022-09-15 15:21:51 +0300

    feat: footer ready

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -4,7 +4,25 @@ import './Footer.css';
 
 function Footer() {
   return (
-    <p>Footer</p>
+    <footer className='footer'>
+      <div className="section-container">
+        <p className='footer__text'>Учебный проект Яндекс.Практикум х BeatFilm.</p>
+        <div className='footer__content'>
+          <div className='footer__content_group'>&copy; 2022</div>
+          <div className='footer__content_group'>
+            <ul className='footer__link-list'>
+              <li className='footer__link-item'>
+                Яндекс.Практикум
+              </li>
+              <li className='footer__link-item'>
+                Github
+              </li>
+            </ul>
+          </div>
+
+        </div>
+      </div>
+    </footer>
   );
 }
 
