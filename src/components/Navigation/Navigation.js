aca5be764fe4bea9bbd5e7dd0fc92373524fc069
Sergei <sergei.ampilov@gmail.com>
2022-09-18 11:42:04 +0300

    feat: search shorts button ready

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -10,7 +10,7 @@ function Navigation({ loggedIn }) {
     <>
       { loggedIn && (
         <nav className='navigation'>
-          <div classname='navigation__group_film'>
+          <div className='navigation__group_film'>
             <a className='navigation__link navigation__link_movie' href='#'>Фильмы</a>
             <a className='navigation__link navigation__link_movie' href='#'>Сохраненные фильмы</a>
           </div>
--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -6,6 +6,15 @@ import breakIcon from '../../images/vertical-break-icon.svg';
 
 
 function SearchForm() {
+  
+  const [searchShorts, setSearchShorts] = React.useState(true);
+
+  const handlecClickSearchShorts = () => {
+    setSearchShorts(!searchShorts);
+  };
+
+  const searchShortsButtonClassName = `${searchShorts ? 'search-form__shorts-button_enabled' : 'search-form__shorts-button_disabled'}`;
+  
   return (
     <div className='search-form'>
       <form className='search-form__form'>
@@ -13,8 +22,12 @@ function SearchForm() {
         <input placeholder='Фильм' className='search-form__input'></input>
         <button type='submit' className='search-form__search-button'></button>
         <img src={breakIcon} alt="Поиск" className="search-form__break_icon"/>
-        <input type="checkbox" id='shorts'></input><label for='shorts'>Короткометражки</label>
-        
+        <button
+          type='button'
+          onClick={handlecClickSearchShorts}
+          className={`search-form__shorts-button ${searchShortsButtonClassName}`}
+          ></button>
+        <label htmlFor='shorts'>Короткометражки</label>        
       </form>
     </div>
   );
