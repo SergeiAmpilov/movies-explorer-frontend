9bee29da2d7ba7a6f1f614e33afe5c97739d46b3
Sergei <sergei.ampilov@gmail.com>
2022-09-28 00:15:05 +0300

    fix semantic

--- src/components/SavedMovies/SavedMovies.js
+++ src/components/SavedMovies/SavedMovies.js
@@ -12,20 +12,64 @@ import Footer from '../Footer/Footer';
 
 function SavedMovies({ handleBurger }) {
 
+  const cardList = [
+    {
+      image: {
+        url: '/uploads/552242179_1280x720_66bc43b289.jpeg'
+      },
+      nameRU: '33 слова о дизайне',
+      duration: 61,
+      id: 1,
+    },
+    {
+      image: { url: '/uploads/552242179_1280x720_66bc43b289.jpeg' },
+      nameRU: '33 слова о дизайне 33 слова о дизайне 33 слова о дизайне 33 слова о дизайне',
+      duration: 161,
+      id: 2,
+    },
+    {
+      image: { url: '/uploads/552242179_1280x720_66bc43b289.jpeg' },
+      nameRU: '34 слова о дизайне',
+      duration: 21,
+      id: 3,
+    },
+    {
+      image: { url: '/uploads/552242179_1280x720_66bc43b289.jpeg' },
+      nameRU: '33 слова о дизайне',
+      duration: 61,
+      id: 4,
+    },
+    {
+      image: { url: '/uploads/552242179_1280x720_66bc43b289.jpeg' },
+      nameRU: '33 слова о дизайне',
+      duration: 61,
+      id: 5,
+    },
+    {
+      image: { url: '/uploads/552242179_1280x720_66bc43b289.jpeg' },
+      nameRU: '34 слова о дизайне',
+      duration: 61,
+      id: 6,
+    },
+  ];
+
   const showPreloader = () => { console.log('showPreloader')};
   const hidePreloader = () => { console.log('hidePreloader')};
   const showEmptyQuery = () => { console.log('showEmptyQuery')};
   return (
-    <section className='movies saved-movies'>
-        <Header loggedIn={true} handleBurger={handleBurger} />
+    <>
+      <Header loggedIn={true} handleBurger={handleBurger} />
+      <main className='movies saved-movies'>
         <SearchForm 
           showPreloader={showPreloader}
           hidePreloader={hidePreloader}
           showEmptyQuery={showEmptyQuery}
         />
-        <MoviesCardList isSaved={true} />
-        <Footer />
-    </section>
+        <MoviesCardList isSaved={true} movieCardList={cardList}  />
+      </main>
+      <Footer />
+
+    </>
   );
 }
 
