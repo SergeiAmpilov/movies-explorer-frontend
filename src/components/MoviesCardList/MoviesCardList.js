ee16c0878499cc24ebf6fb6c8cc09c35ded80d40
Sergei <sergei.ampilov@gmail.com>
2022-09-27 10:18:04 +0300

    refactor

--- src/components/MoviesCardList/MoviesCardList.js
+++ src/components/MoviesCardList/MoviesCardList.js
@@ -11,48 +11,6 @@ import api from '../../utils/api';
 
 function MoviesCardList({ isSaved, isPreloaderVisible = false, movieCardList = [] }) {
 
-  /*
-  const cardList = [
-    {
-      img: 'https://images.unsplash.com/photo-1662499840292-0805bbc95005?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=987&q=80',
-      title: '33 слова о дизайне',
-      duration: 61,
-      _id: 1,
-    },
-    {
-      img: 'https://images.unsplash.com/photo-1662499840292-0805bbc95005?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=987&q=80',
-      title: '33 слова о дизайне 33 слова о дизайне 33 слова о дизайне 33 слова о дизайне',
-      duration: 161,
-      _id: 2,
-    },
-    {
-      img: 'https://images.unsplash.com/photo-1663431262170-b94c02b712cd?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=987&q=80',
-      title: '34 слова о дизайне',
-      duration: 21,
-      _id: 3,
-    },
-    {
-      img: 'https://images.unsplash.com/photo-1662499840292-0805bbc95005?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=987&q=80',
-      title: '33 слова о дизайне',
-      duration: 61,
-      _id: 4,
-    },
-    {
-      img: 'https://images.unsplash.com/photo-1662499840292-0805bbc95005?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=987&q=80',
-      title: '33 слова о дизайне',
-      duration: 61,
-      _id: 5,
-    },
-    {
-      img: 'https://images.unsplash.com/photo-1663431262170-b94c02b712cd?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=987&q=80',
-      title: '34 слова о дизайне',
-      duration: 61,
-      _id: 6,
-    },
-
-  ];
-  */ 
-
   const cardsElements = movieCardList.map( card => <MoviesCard 
     img={`${api.getSiteUrl()}${card.image.url}`}
     key={card.id}
