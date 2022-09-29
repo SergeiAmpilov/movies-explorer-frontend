133696c31b65803345903514c6d13c8f96555617
Sergei <sergei.ampilov@gmail.com>
2022-09-29 15:55:51 +0300

    fix links in portfolio

--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -11,19 +11,19 @@ function Portfolio() {
         <h4 className='portfolio__small-title'>Портфолио</h4>
         <ul className='portfolio__case-list'>
           <li className='portfolio__list-element'>
-            <a className='portfolio__case-item' href='/' target='_blank'>
+            <a className='portfolio__case-item' href='https://github.com/SergeiAmpilov/how-to-learn' target='_blank' rel="noreferrer">
               <div className='portfolio__case-item_text'>Статичный сайт</div>
               <img src={arrowMain} alt="Стрелка" className="portfolio__case-item_img"/>
             </a>
           </li>
           <li className='portfolio__list-element'>
-            <a className='portfolio__case-item' href='/' target='_blank'>
+            <a className='portfolio__case-item' href='https://sergeiampilov.github.io/russian-travel/' target='_blank' rel="noreferrer">
               <div className='portfolio__case-item_text'>Адаптивный сайт</div>
               <img src={arrowMain} alt="Стрелка" className="portfolio__case-item_img"/>            
             </a>
           </li>
           <li className='portfolio__list-element'>
-            <a className='portfolio__case-item' href='/' target='_blank'>
+            <a className='portfolio__case-item' href='https://mesto.ampilovs.ru/' target='_blank' rel="noreferrer">
               <div className='portfolio__case-item_text'>Одностраничное приложение</div>
               <img src={arrowMain} alt="Стрелка" className="portfolio__case-item_img"/>            
             </a>
