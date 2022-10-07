f6c4e6d8c29db95cc84fffe135633dca4420040a
Sergei <sergei.ampilov@gmail.com>
2022-10-07 10:50:12 +0300

    fix go back button

--- src/components/PageNotFound/PageNotFound.js
+++ src/components/PageNotFound/PageNotFound.js
@@ -1,14 +1,21 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
 
 import './PageNotFound.css';
 
 function PageNotFound() {
+
+  const history = useHistory();
+  const handleClickGoBack = () => {
+    history.goBack();
+  }
+
   return (
     <section className='not-found'>
       <p className='not-found__text-404'>404</p>
       <p className='not-found__text-notfound'>Страница не найдена</p>
       <p className='not-found__link-section'></p>
-      <a href='#' className='not-found__link'>Назад</a>
+      <button className='not-found__link' onClick={handleClickGoBack}>Назад</button>
     </section>
   );
 }
