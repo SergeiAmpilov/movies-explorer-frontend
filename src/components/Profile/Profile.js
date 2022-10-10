515947711cdd80600cc5e20687a5bae13569dcf7
Sergei <sergei.ampilov@gmail.com>
2022-10-10 14:20:15 +0300

    fix browser validation

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -3,6 +3,7 @@ import { Link } from 'react-router-dom';
 
 import { currentUserContext } from '../../contexts/CurrentUserContext';
 import { MESSAGES } from '../../utils/constants';
+import isEmail from 'validator/es/lib/isEmail';
 
 
 import './Profile.css';
@@ -19,10 +20,22 @@ function Profile({ handleLogout, handleUpdate, openPopup }) {
 
   const [isVisibleButton, setVisibleButton] = React.useState(false);
 
+  // const [inputValues, setInputValues] = React.useState({});
+  const [errors, setErrors] = React.useState({});
+  const [isValid, setIsValid] = React.useState(false);
 
   const handleChangeName = (evt) => {
-    const value = evt.target.value;
+    const target = evt.target;
+    const name = target.name;
+    const value = target.value;
+
+    // const value = evt.target.value;
+
+
     setName(value);
+
+    setErrors({ ...errors, [name]: target.validationMessage });
+    setIsValid(target.closest('form').checkValidity());
 
     if (value !== lastName) {
       setVisibleButton(true);
@@ -30,8 +43,19 @@ function Profile({ handleLogout, handleUpdate, openPopup }) {
       setVisibleButton(false);
     }
   };
+
   const handleChangeEmail = (evt) => {
-    const value = evt.target.value;
+    const target = evt.target;
+    const name = target.name;
+    const value = target.value;
+
+    // const value = evt.target.value;
+    if (!isEmail(value)) {
+      target.setCustomValidity('Некорректый адрес почты.');
+    } else {
+      target.setCustomValidity('');
+    }
+
     setEmail(value);
 
     if (value !== lastEmail) {
@@ -39,6 +63,8 @@ function Profile({ handleLogout, handleUpdate, openPopup }) {
     } else {
       setVisibleButton(false);
     }
+    setErrors({ ...errors, [name]: target.validationMessage });
+    setIsValid(target.closest('form').checkValidity());
   };
 
   const handleLogoutClick = (evt) => {
@@ -77,10 +103,13 @@ function Profile({ handleLogout, handleUpdate, openPopup }) {
             minLength="2"
             maxLength="40"
             onChange={handleChangeName}
-            value={name}
+            value={name || ''}
             required
           />
         </label>
+        <p className={`register__form_field-error name-input-error ${errors.name ? 'register__form_field-error_visible' : '' }`}>
+          {errors.name}
+        </p>
         <label className='profile__form_group'>
           <p className='profile__form_title'>E-mail</p>
           <input
@@ -90,12 +119,15 @@ function Profile({ handleLogout, handleUpdate, openPopup }) {
             id="email-input"
             type='email'
             onChange={handleChangeEmail}
-            value={email}
+            value={email || ''}
             required
           />
         </label>
+        <p className={`register__form_field-error email-input-error ${errors.email ? 'register__form_field-error_visible' : '' }`}>
+          {errors.email}
+        </p>
         <div className='profile__form_footer'>
-          { isVisibleButton && 
+          { isVisibleButton && isValid &&
             <button
               className='profile__form_button'
               type='submit'
